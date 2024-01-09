df64efb117993ce2d49d70632f1a323cfff66012
Kaden Wingert <kaden.wingert@gmail.com>
2024-01-09 17:34:50 -0600

    worked on modal

--- nodemailer/index.ts
+++ nodemailer/index.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { EmailContent, EmailProductInfo, NotificationType } from "@/types";
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
 const Notification = {
   WELCOME: "WELCOME",
@@ -80,31 +80,31 @@ export async function generateEmailBody(
   return { subject, body };
 }
 
-// const transporter = nodemailer.createTransport({
-//   pool: true,
-//   service: "hotmail",
-//   port: 2525,
-//   auth: {
-//     user: "javascriptmastery@outlook.com",
-//     pass: process.env.EMAIL_PASSWORD,
-//   },
-//   maxConnections: 1,
-// });
+const transporter = nodemailer.createTransport({
+  pool: true,
+  service: "hotmail",
+  port: 2525,
+  auth: {
+    user: "javascriptmastery@outlook.com",
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  maxConnections: 1,
+});
 
-// export const sendEmail = async (
-//   emailContent: EmailContent,
-//   sendTo: string[]
-// ) => {
-//   const mailOptions = {
-//     from: "javascriptmastery@outlook.com",
-//     to: sendTo,
-//     html: emailContent.body,
-//     subject: emailContent.subject,
-//   };
+export const sendEmail = async (
+  emailContent: EmailContent,
+  sendTo: string[]
+) => {
+  const mailOptions = {
+    from: "javascriptmastery@outlook.com",
+    to: sendTo,
+    html: emailContent.body,
+    subject: emailContent.subject,
+  };
 
-//   transporter.sendMail(mailOptions, (error: any, info: any) => {
-//     if (error) return console.log(error);
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if (error) return console.log(error);
 
-//     console.log("Email sent: ", info);
-//   });
-// };
+    console.log("Email sent: ", info);
+  });
+};
